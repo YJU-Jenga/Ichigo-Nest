240d39f82f60d5b008314c2d2b1789e1492f0fab
ckcic <ckcicic@gmail.com>
2023-02-17 14:02:58 +0900

    docker setting

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -3,10 +3,10 @@ import { Board, Cart, Comment, Order, Post, Product, User } from 'src/model/enti
 
 export const TypeOrmConfig: TypeOrmModule = {
   type: 'mysql',
-  host: 'localhost',
+  host: 'db',
   port: 3306,
-  username: 'root',
-  password: 'password',
+  username: 'username',
+  password: 'mypassword',
   database: 'ichigo',
   entities: [User, Board, Post, Comment, Product, Cart, Order], // Entity 연결
   synchronize: true,  //true 값을 설정하면 어플리케이션을 다시 실행할 때 엔티티안에서 수정된 컬럼의 길이 타입 변경값등을 해당 테이블을 Drop한 후 다시 생성해준다,
