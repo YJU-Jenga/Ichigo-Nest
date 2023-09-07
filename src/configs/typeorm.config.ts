fbe2e8f17c3260332a441ee8a1f75848581cc30a
ckcic <ckcicic@gmail.com>
2023-09-07 11:05:58 +0900

    typeorm.comfig fix use env

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -3,11 +3,11 @@ import { Board, Calendar ,Cart, Comment, PurchaseOrder, Post, Product, User, Car
 
 export const TypeOrmConfig: TypeOrmModule = {
   type: 'mysql', // データベース設定
-  host: 'db',
+  host: process.env.MYSQL_SERVER, //'db',
   port: 3306,
-  username: 'username',
-  password: 'mypassword',
-  database: 'ichigo',
+  username: process.env.MYSQL_USER, // 'username',
+  password: process.env.MYSQL_PASSWORD, // 'mypassword',
+  database: process.env.MYSQL_DATABASE, // 'ichigo',
   entities: [Board, Calendar ,Cart, Comment, PurchaseOrder, Post, Product, User, CartToProduct, OrderToProduct, Device, Alarm, Music, Clothes, CartToProductOption, Models, OrderToProductOption], // Entityをつなぐ
   synchronize: true,  //trueの値を設定すると、アプリケーションを再実行する時、エンティティで修正されたカラムの長さタイプ変更値などを該当テーブルをDropして再作成してくれます
 };
